218612a2e6e4fd8fb558ed613f4fd92150620dd4
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-10 23:05:02 -0500

    feat: Initial params for manager services

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-  std::cout << "Hello World" << std::endl;
-  system("pause");
+  if (argc == 1 || argc != 3)
+  {
+    cout << "Type help command" << endl;
+    return 1;
+  }
+
+  string action = argv[1];
+  string content = argv[2];
+
+  string services[3] = {"OracleServiceXE", "OracleXETNSListener", "TeamViewer"};
+  int sizeServices = sizeof(services) / sizeof(services[0]);
+
+  if (action == "start")
+  {
+    for (int i = 0; i < sizeServices; i++)
+    {
+      string command = "net start " + services[i];
+      system(command.c_str());
+    }
+  }
+  else if (action == "stop")
+  {
+    for (int i = 0; i < sizeServices; i++)
+    {
+      string command = "net stop " + services[i];
+      system(command.c_str());
+    }
+  }
+  else if (action == "config")
+  {
+    for (int i = 0; i < sizeServices; i++)
+    {
+      string command = "sc config " + services[i] + " start=demand";
+      system(command.c_str());
+    }
+  }
+
+  return 0;
 }
